f3fe89dffad42616bc386860e5573ac65ed07eb2
PatrickMatias <patrick@flags.hk>
2016-02-03 17:45:48 +0800

    fixed error

--- wp-content/themes/blank/header.php
+++ wp-content/themes/blank/header.php
@@ -41,7 +41,7 @@
 	        	<span class="icon-bar"></span>
 	       		<span class="icon-bar"></span>
 			</button>
-	     	 <a class="navbar-brand" href="/">TwilioSMS</a>
+	     	 <a class="navbar-brand" href="#">TwilioSMS</a>
 	    </div>
 		<div class="collapse navbar-collapse" id="actionMenu">
 			<ul class="nav navbar-nav">
